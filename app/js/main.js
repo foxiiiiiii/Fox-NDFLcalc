bf37d1f06babd4c234a816c3243ca28bc5de4851
Fox-Foxi <gold.gold275@gmail.com>
2023-12-22 17:16:23 +0500

    5

--- app/js/main.js
+++ app/js/main.js
@@ -5,15 +5,15 @@ $(function () {
 
 
 $(window).on('load', function() {
-    var slider = $(".slider__items");
+    var slider = $(".slider-history__items");
 
-    if (!slider.hasClass('slick-initialized')) {
-        slider.slick({
-            slidesToShow: 5,
-            slidesToScroll: 1,
-            prevArrow: '<button type="button" class="slick-prev"><img src="<svg xmlns=\'http://www.w3.org/2000/svg\' width=\'13\' height=\'25\' viewBox=\'0 0 13 25\' fill=\'none\'><path d=\'M12.7365 3.12253L4.8192 12.4902L12.7365 22.066L10.3269 24.9803L2.42255e-07 12.4902L10.3269 -1.52439e-05L12.7365 3.12253Z\' fill=\'#8BA8BC\'/></svg>" alt="left-arrow.svg"></button>',
-            nextArrow: '<button type="button" class="slick-next"><img src="" alt="right-arrow.svg"></button>',
-            responsive: [
+  if (!slider.hasClass('slick-initialized')) {
+    slider.slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      prevArrow: $('.slider-history__arrow--left'),
+      nextArrow: $('.slider-history__arrow--right'),
+      responsive: [
                 {
                     breakpoint: 1300,
                     settings: {
